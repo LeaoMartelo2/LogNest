e34a92b7f7332ff507acfa1f5c5058a263cfd91f
LeaoMartelo <leaomartelo@gmail.com>
2024-09-11 14:31:44 -0300

    readme update

--- examples/installed_example/installed.cpp
+++ examples/installed_example/installed.cpp
@@ -9,8 +9,8 @@ int main() {
 
     log_example->log(LN_LOG, "Normal Log with 1 argument: {}", "hello");
     log_example->log(LN_WARN, "Warn log with no arguments");
-    log_example->log(LN_ERROR, "Error log with 2 arguments {}, {}", "hello", "world");
-    log_example->log(LN_DEBUG, "Debug log with int arguments {}", 123);
+    log_example->log(LN_ERROR, "{} The arguments can, be anywhere on the message {}", "HELLO", "WORLD");
+    log_example->log(LN_DEBUG, "With almost any type! int: {} float: {}, char: {}, std::string: {}", 1, 5.4f, 'h', "HELLO");
 
     return 0;
 }
